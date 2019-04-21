0651a3abb4693b61672f8d1f608a2f7eb8ecf5a4
lingmujianshi <lingmujianshi@gmail.com>
2019-04-21 16:08:58 +0900

    second

--- main.cpp
+++ main.cpp
@@ -29,6 +29,15 @@ public:
 
     void putStr(std::string str)
     {
+        int p = static_cast<int>(str.find('.'));
+        if (p<0) p = 0;
+        int point = str.size() - p -1;
+        int lack = decimalPlace - point;
+        if (lack > 0)
+        for (int i = 0; i < lack; i++)
+        {
+            str.insert(str.end(),'0');
+        }
         
         str.erase(std::find(str.begin(),str.end(),'.'));
         data = std::atoi(str.c_str());
@@ -39,9 +48,11 @@ int main()
 {
     Word p(23111);
     std::cout << p.getStr() << std::endl;
-    p.putStr("1.234");
-    std::cout << p.getStr() << std::endl;
-    p.putStr("1.230");
+    // p.putStr("1.234");
+    // std::cout << p.getStr() << std::endl;
+    // p.putStr("1.23");
+    // std::cout << p.getStr() << std::endl;
+    p.putStr("1");
     std::cout << p.getStr() << std::endl;
     return 0;
 }
